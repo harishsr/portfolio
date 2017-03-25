770ece5eae9b9411f8129dcaaacebe51ec391825
Harish Ramachandran <harishsr@icloud.com>
2017-03-25 15:39:08 -0400

    Using shoulda-matchers, start planning tests for Admin

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,13 +1,9 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  # TESTS
-  #
-  # should require a username
-  # username should be unique (regardless of case)
-  # should require a first_name and last_name
+  context 'validations' do
+    should 'require a username'
+    should 'require username be unique'
+    should 'require a first_name and last_name'
+  end
 end

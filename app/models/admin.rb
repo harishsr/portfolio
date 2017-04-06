75693185f8bb835d77fe1e195c746c68c4955bb6
Harish Ramachandran <harishsr@icloud.com>
2017-04-05 22:25:49 -0400

    Admin should have many entries

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,7 @@
 class Admin < ApplicationRecord
+
+  has_many :entries
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

94289c8b7508dc0efc57b4d2ccdc5f00cf0a9c52
Harish Ramachandran <harishsr@icloud.com>
2017-04-08 16:27:38 -0400

    Correct silly mistake in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :entry
+  resources :entries
   root 'entries#index'
 end

69e9067d3df64c911016639b216c648108434652
Harish Ramachandran <harishsr@icloud.com>
2017-04-08 16:59:02 -0400

    clean routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :entries
   root 'entries#index'
+  resources :entries
 end

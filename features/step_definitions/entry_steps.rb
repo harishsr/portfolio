2426d918a904e59d6b9b10b3fcd7e165d3e17045
Harish Ramachandran <harishsr@icloud.com>
2017-04-14 18:03:30 -0400

    Feature step to upload an image

--- features/step_definitions/entry_steps.rb
+++ features/step_definitions/entry_steps.rb
@@ -4,3 +4,8 @@ Given /^"(.+)" has an entry titled "(.+)" with content "(.+)"$/ do |username, ti
 
   FactoryGirl.create :entry, admin: admin, title: title, content: content
 end
+
+When /^I attach a valid image$/ do
+  file = File.join(Rails.root, 'test/files/valid_image.jpeg')
+  attach_file :entry_image, file
+end

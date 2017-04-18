545e8530d73c45f2bf7378eb4171e17f0f576e53
Harish Ramachandran <harishsr@icloud.com>
2017-04-18 17:58:11 -0400

    Sanitize unsanitized params for registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :collection, :resource
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_params
+    keys = %i(username first_name last_name email)
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+  end
 end

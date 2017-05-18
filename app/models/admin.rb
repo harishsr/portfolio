a0e28a5fc473ca9c76a6839cd93b3a5046207803
Harish Ramachandran <harishsr@icloud.com>
2017-05-17 20:31:11 -0400

    Rubocop on the models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
-class Admin < ApplicationRecord
+# frozen_string_literal: true
 
+class Admin < ApplicationRecord
   has_many :entries, dependent: :destroy
 
   # Include default devise modules. Others available are:
@@ -7,13 +8,13 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, 
-    presence: true, 
-    length: { minimum: 6 },
-    uniqueness: { case_sensitive: false }
-  validates :first_name, :last_name, 
-    presence: true, 
-    length: { in: 2..25 }
+  validates :username,
+            presence: true,
+            length: { minimum: 6 },
+            uniqueness: { case_sensitive: false }
+  validates :first_name, :last_name,
+            presence: true,
+            length: { in: 2..25 }
 
   def name
     first_name + ' ' + last_name

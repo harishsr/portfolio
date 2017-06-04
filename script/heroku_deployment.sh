d4e2040ca63d5e7b90cde5badbd1445b83508744
Harish Ramachandran <harishsr@icloud.com>
2017-06-04 12:23:53 -0400

    Script to deploy to Heroku

---
+++ script/heroku_deployment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Pushing changes to Heroku"
+git push heroku master
+
+echo "Migrating Heroku's Postgres db"
+heroku run rake db:migrate
+
+echo "Restarting Heroku"
+heroku restart
